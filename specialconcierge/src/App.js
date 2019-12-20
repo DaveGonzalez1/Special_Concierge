ce4338728848245e4839a8b518227701fec25ac6
cerox325 <cerox325>
2019-12-20 10:38:18 -0600

    Version React 3

--- specialconcierge/src/App.js
+++ specialconcierge/src/App.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
+import BarraLateral from './components/BarraLateral'
+import Comentario from './components/Comentarios'
+import Informacion from './components/Informacion'
 import './App.css';
-function App() {
+import ReactFontFace from 'react-font-face'
+import Font from './components/fonts/Gibson-SemiBold.otf'
+const styles = {
+  main: {
+    margin: 15,
+    lineHeight: 1.4,
+    fontFamily: 'Gibson',
+  },
+  google1: {
+    fontFamily: 'Pangolin',
+  },
+  google2: {
+    fontFamily: 'Roboto Mono',
+  },
+};
+export default class App extends React.Component {
+ render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-         
-        </p>
-      </header>
+    <div tyle={ styles.google1 } >
+         {/* <Comentario/> */}
+         <BarraLateral/>
+         {/* <Informacion/> */}
+       
     </div>
   );
+ }
 }
-
-export default App;
--- specialconcierge/src/index.js
+++ specialconcierge/src/index.js
@@ -2,13 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import BarraLateral from './components/BarraLateral'
-import Comentario from './components/Comentarios'
-import Informacion from './components/Informacion'
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<Informacion />, document.getElementById('root'));
-// ReactDOM.render(<Comentario />, document.getElementById('root'));
-// ReactDOM.render(<BarraLateral />, document.getElementById('root'));
+ReactDOM.render(
+    <App />, document.getElementById('root')
+);
 
 serviceWorker.unregister();
